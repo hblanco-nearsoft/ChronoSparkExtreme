e8ea84eab171bef029221ee90aedb51efdf074b0
Carlos Nunez <rnunez@nearsoft.com>
2012-10-09 14:46:21 -0700

    start of data layer, task poco started.

---
+++ ChronoSpark.Data.Entities/Task.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChronoSpark.Data.Entities
+{
+    public class Task
+    {
+        public string ID { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ ChronoSpark.Data/Repository.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Raven.Client.Document;
+using Raven.Client.Connection;
+using Raven.Client.Embedded;
+
+
+namespace ChronoSpark.Data
+{
+    public interface IRepository
+    {
+        bool Add<T>(T task);
+    }
+
+    public class Repository
+    { 
+        private DocumentStore Docstore;
+
+        public bool Initialize() {
+            Docstore = new EmbeddableDocumentStore{ConnectionStringName="RavenDB", UseEmbeddedHttpServer=true}; // connection string where art thou?
+        Docstore.Initialize();
+        return true;
+        }
+
+        public bool Cleanup() {
+            Docstore.Dispose();
+            return true;
+        }
+        
+        public bool Add<T>(T Task)
+        {
+                using (var Session = Docstore.OpenSession())
+                {
+                    Session.Store(Task);
+                    Session.SaveChanges();
+                }
+            return true;
+        }
+
+        public bool Update<T>(T Task)
+        {
+            using (var Session = Docstore.OpenSession())
+            {
+                String TaskID;
+                TaskID = "Task";  //get the id
+                var task = Session.Load<Task>(TaskID);
+                Session.Delete(Task);
+                Session.SaveChanges();
+            
+            }
+
+            return true;
+        }
+
+        public bool Delete<T>(T Task)
+        {
+            using (var Session = Docstore.OpenSession())
+            {
+                String TaskID;
+                TaskID = "Task";
+                var task = Session.Load<Task>(TaskID);
+            }
+               return true;
+        }
+
+        }
+    }
